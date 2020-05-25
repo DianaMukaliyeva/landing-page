9fc5b51c94912c6123a60dda733c15d547e9da0b
Diana <diana.mukaliyeva@gmail.com>
2020-05-26 00:49:40 +0300

    Implement highlight selected section and menu

--- js/app.js
+++ js/app.js
@@ -16,51 +16,97 @@
 /**
  * Global Variables
 */
-const navigationMenu = document.querySelector('.navbar__menu');
-const navigationList = document.querySelector('#navbar__list');
+
+const navigationMenu = document.querySelector('#navbar__list');
 const sections = document.querySelectorAll('section');
 
 /**
  * Helper Functions
 */
 
+/**
+* Scroll to section selected in navigational menu.
+* @param {event} event
+*/
+function scrollToSection(event) {
+    event.preventDefault();
+    const id = event.target.getAttribute('href');
+    const element = document.querySelector(`#${id}`);
+    element.scrollIntoView({behavior: 'smooth'});
+}
+
+/**
+ * Get 'id' of the section that is in the viewport.
+ * @returns {string} 'id' of active section.
+*/
+function getActiveSectionID() {
+    const views = [];
+    sections.forEach(section => {
+        const top = section.getBoundingClientRect().top;
+        views.push({id: section.getAttribute('id'), top: Math.abs(top)});
+    });
+    views.sort(function(a, b) {
+        return a.top - b.top;
+    });
+    return views[0].id;
+}
+
+/**
+ * Add or remove class 'active-class' according to giveb attribute and value.
+ * @param {NodeList} objects
+ * @param {string} attr
+ * @param {string} value
+*/
+function addActiveClass(objects, attr, value) {
+    objects.forEach(obj => {
+        if (obj.getAttribute(attr) == value) {
+            obj.classList.add('active-class');
+        } else {
+            obj.classList.remove('active-class');
+        }
+    });
+}
 
 /**
  * Main Functions
 */
 
-// build the nav
+/** Build a dynamically updating navigational menu. */
 function buildMenu() {
-    for (const section of sections) {
+    sections.forEach(section => {
         const liItem = document.createElement('li');
-        liItem.className = 'menu__link';
-        liItem.innerText = section.querySelector('h2').innerText;
-        liItem.setAttribute('href', `#${section.getAttribute('id')}`)
-        navigationList.appendChild(liItem);
-    }
+        const linkItem = document.createElement('a');
+        linkItem.className = 'menu__link';
+        linkItem.innerText = section.getAttribute('data-nav');
+        linkItem.setAttribute('href', section.getAttribute('id'));
+        liItem.appendChild(linkItem);
+        navigationMenu.appendChild(liItem);
+    });
 }
 
-// Add class 'active' to section when near top of viewport
-function scrollToSection() {
-    const navigationMenu = document.querySelector('.navbar__menu');
-    navigationMenu.addEventListener('click', scrollTo(event));
+/**
+ * Set class 'active-class' to section and navigation menu.
+*/
+function setActiveSection() {
+    activeSectionID = getActiveSectionID();
+    const navigationLinks = navigationMenu.querySelectorAll('li a');
+    addActiveClass(sections, 'id', activeSectionID);
+    addActiveClass(navigationLinks, 'href', activeSectionID);
 }
 
-
-// Scroll to anchor ID using scrollTO event
-function scrollToSection(event) {
-    const target = event.target.getAttribute('href');
-    const element = document.querySelector(target);
-    element.scrollIntoView();
-    window.scrollBy(0, -1 * navigationMenu.offsetHeight);
+/** Scroll to section selected in navigation menu. */
+function scrollToSelectedSection() {
+    const navigationLinks = navigationMenu.querySelectorAll('li a');
+    navigationLinks.forEach(link => {
+        link.addEventListener('click', scrollToSection);
+    });
 }
 
-// Build menu 
+// Build menu
 buildMenu();
 
 // Scroll to section on link click
-navigationMenu.addEventListener('click', scrollToSection);
+scrollToSelectedSection();
 
 // Set sections as active
-
-
+document.addEventListener('scroll', setActiveSection);
