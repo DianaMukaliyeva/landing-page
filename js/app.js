2d1fa756b9dccc308fd71126f0665eb3e5e827bb
Diana <diana.mukaliyeva@gmail.com>
2020-05-25 17:05:02 +0300

    Implemented navigation menu and scrolling
    Working on different approach of scrolling

--- js/app.js
+++ js/app.js
@@ -14,43 +14,52 @@
 */
 
 /**
- * Define Global Variables
- * 
+ * Global Variables
+*/
+const navigationMenu = document.querySelector('.navbar__menu');
+const navigationList = document.querySelector('#navbar__list');
+const sections = document.querySelectorAll('section');
+
+/**
+ * Helper Functions
 */
 
 
 /**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
+ * Main Functions
 */
 
 // build the nav
-
+function buildMenu() {
+    for (const section of sections) {
+        const liItem = document.createElement('li');
+        liItem.className = 'menu__link';
+        liItem.innerText = section.querySelector('h2').innerText;
+        liItem.setAttribute('href', `#${section.getAttribute('id')}`)
+        navigationList.appendChild(liItem);
+    }
+}
 
 // Add class 'active' to section when near top of viewport
+function scrollToSection() {
+    const navigationMenu = document.querySelector('.navbar__menu');
+    navigationMenu.addEventListener('click', scrollTo(event));
+}
 
 
 // Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
+function scrollToSection(event) {
+    const target = event.target.getAttribute('href');
+    const element = document.querySelector(target);
+    element.scrollIntoView();
+    window.scrollBy(0, -1 * navigationMenu.offsetHeight);
+}
 
 // Build menu 
+buildMenu();
 
 // Scroll to section on link click
+navigationMenu.addEventListener('click', scrollToSection);
 
 // Set sections as active
 
